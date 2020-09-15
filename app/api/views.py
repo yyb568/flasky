a0f1d05d85d0c409ff31a9e26798c6213c01c156
yyb <121831104@qq.com>
2020-09-15 18:10:10 +0800

    add

--- app/api/views.py
+++ app/api/views.py
@@ -72,9 +72,7 @@ def get_dou_yin(user_agent, link):
 
     return video_url
 
-
-
-def get_kuai_shou(user_agent, link):
+def get_kuai_shou(link):
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
